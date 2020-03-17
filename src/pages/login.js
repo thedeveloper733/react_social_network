9f0859a13455ab9a81cfe13df54e17744fb561b2
Robert Veres <thedeveloper733@gmail.com>
2020-03-16 21:30:45 -0400

    Implementing reducers

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import axios from "axios";
 
+// Redux stuff
+import { connect } from "react-redux";
+import { loginUser } from "../redux/actions/userActions";
+
 import withStyles from "@material-ui/core/styles/withStyles";
 
 // MUI Stuff
@@ -24,7 +28,6 @@ class Login extends Component {
     this.state = {
       email: "",
       password: "",
-      loading: false,
       errors: {}
     };
   }
@@ -37,34 +40,19 @@ class Login extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    this.setState({
-      loading: true
-    });
     const userData = {
       email: this.state.email,
       password: this.state.password
     };
-    axios
-      .post("/login", userData)
-      .then(res => {
-        console.log(res.data);
-        localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
-        this.setState({
-          loading: false
-        });
-        this.props.history.push("/");
-      })
-      .catch(err => {
-        this.setState({
-          errors: err.response.data,
-          loading: false
-        });
-      });
+    this.props.loginUser(userData, this.props.history);
   };
 
   render() {
-    const { classes } = this.props;
-    const { errors, loading } = this.state;
+    const {
+      classes,
+      UI: { loading }
+    } = this.props;
+    const { errors } = this.state;
     return (
       <Grid container className={classes.form}>
         <Grid item sm />
@@ -128,7 +116,22 @@ class Login extends Component {
 }
 
 Login.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  loginUser: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Login);
+const mapStateToProps = state => ({
+  user: state.user,
+  UI: state.UI
+});
+
+const mapActionsToProps = {
+  loginUser
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(styles)(Login));
---
+++ src/redux/actions/userActions.js
@@ -0,0 +1,41 @@
+import {
+  SET_USER,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI,
+  SET_AUTHENTICATED,
+  SET_UNAUTHENTICATED
+} from "../types";
+import axios from "axios";
+
+export const loginUser = (userData, history) => dispatch => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post("/login", userData)
+    .then(res => {
+      localStorage.setItem("FBIdToken", `Bearer ${res.data.token}`);
+      const FBIdToken = `Bearer ${res.data.token}`;
+      axios.defaults.headers.common["Authorization"] = FBIdToken;
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      this.props.history.push("/");
+    })
+    .catch(err => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+export const getUserData = () => dispatch => {
+  axios
+    .get("/user")
+    .then(res => {
+      dispatch({
+        type: SET_USER,
+        payload: res.data
+      });
+    })
+    .catch(err => console.log(err));
+};
