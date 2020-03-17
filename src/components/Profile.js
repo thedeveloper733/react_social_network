e709c31c9757c748bb610723085d31c7a88e80d6
Robert Veres <thedeveloper733@gmail.com>
2020-03-17 14:29:53 -0400

    Adding logout button

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -23,6 +23,7 @@ import LocationOn from "@material-ui/icons/LocationOn";
 import LinkIcon from "@material-ui/icons/Link";
 import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
 import EditIcon from "@material-ui/icons/Edit";
+import KeyboardReturn from "@material-ui/icons/KeyboardReturn";
 
 const styles = theme => ({
   paper: {
@@ -85,6 +86,10 @@ class Profile extends Component {
     fileInput.click();
   };
 
+  handleLogout = () => {
+    this.props.logoutUser();
+  };
+
   render() {
     const {
       classes,
@@ -145,6 +150,11 @@ class Profile extends Component {
               <CalendarTodayIcon color="primary" />
               <span>Joined {dayjs(createdAt).format("MMM YYYY")}</span>
             </div>
+            <Tooltip title="Logout" placement="top">
+              <IconButton onClick={this.handleLogout}>
+                <KeyboardReturn color="primary" />
+              </IconButton>
+            </Tooltip>
           </div>
         </Paper>
       ) : (
