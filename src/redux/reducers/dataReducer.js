229c9cc6184da6aabf5cbadda1bddda928963da5
Robert Veres <thedeveloper733@gmail.com>
2020-03-19 17:24:42 -0400

    Adding delete button

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -2,7 +2,8 @@ import {
   SET_SCREAMS,
   LOADING_DATA,
   LIKE_SCREAM,
-  UNLIKE_SCREAM
+  UNLIKE_SCREAM,
+  DELETE_SCREAM
 } from "../types";
 
 const initialState = {
@@ -33,7 +34,15 @@ export default function(state = initialState, action) {
       return {
         ...state
       };
+    case DELETE_SCREAM:
+      index = state.screams.findIndex(
+        scream => scream.screamId === action.payload
+      );
+      state.screams.splice(index, 1);
+      return {
+        ...state
+      };
     default:
-        return state;
+      return state;
   }
 }
