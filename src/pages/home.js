15f0397a813bd347f44ba23df63c999bd1e63bf7
Robert Veres <thedeveloper733@gmail.com>
2020-03-20 19:49:49 -0400

    Adding Home page skeleton

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 
 import Scream from "../components/scream/Scream";
 import Profile from "../components/profile/Profile";
+import ScreamSkeleton from "../util/ScreamSkeleton";
 
 import { connect } from "react-redux";
 import { getScreams } from "../redux/actions/dataActions";
@@ -19,7 +20,7 @@ class Home extends Component {
     let recentScreamsMarkup = !loading ? (
       screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
     ) : (
-      <p>Loading...</p>
+      <ScreamSkeleton />
     );
 
     return (
--- src/pages/user.js
+++ src/pages/user.js
@@ -8,6 +8,7 @@ import axios from "axios";
 
 import Scream from "../components/scream/Scream";
 import StaticProfile from "../components/profile/StaticProfile";
+import ScreamSkeleton from "../util/ScreamSkeleton";
 
 import Grid from "@material-ui/core/Grid";
 
@@ -40,7 +41,7 @@ export class User extends Component {
     const {screamIdParam} = this.state; 
     
     const screamsMarkup = loading ? (
-      <p>Loading data...</p>
+      <ScreamSkeleton/>
     ) : screams === null ? (
       <p>No screams from this user.</p>
     ) : !screamIdParam ? (
