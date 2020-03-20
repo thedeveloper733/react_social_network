e53e5a3ff1047f98209119a3570fc376e12caf82
Robert Veres <thedeveloper733@gmail.com>
2020-03-20 19:31:28 -0400

    Fixing Notifications proptypes

--- src/components/layout/Notifications.js
+++ src/components/layout/Notifications.js
@@ -126,7 +126,7 @@ export class Notifications extends Component {
 
 Notifications.propTypes = {
   markNotificationsRead: PropTypes.func.isRequired,
-  notifications: PropTypes.object.isRequired
+  notifications: PropTypes.array.isRequired
 };
 
 const mapStateToProps = state => ({
